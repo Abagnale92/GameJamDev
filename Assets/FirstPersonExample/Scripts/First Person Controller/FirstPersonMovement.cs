5c5303295a74c7a1a3f64fdb615bb3764a29ccc3
Peppe Abagnale <34139465+Abagnale92@users.noreply.github.com>
2023-05-24 12:51:57 +0200

    add movement, camera follow, and asset example

---
+++ Assets/FirstPersonExample/Scripts/First Person Controller/FirstPersonMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 5;
+    Vector2 velocity;
+    Vector2 rawInput;
+
+    void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        velocity.x = rawInput.x * speed * Time.deltaTime;
+        velocity.y = rawInput.y * speed * Time.deltaTime;
+        transform.Translate(velocity.x, 0, velocity.y);
+    }
+
+    //void OnMove(InputValue value)
+    //{
+    //    rawInput = value.Get<Vector2>();
+    //}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f; // Velocità di movimento del personaggio
+    Vector3 movement;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(-movement, Vector3.up);
+            rb.MoveRotation(Quaternion.Lerp(rb.rotation, toRotation, 0.5f));
+        }
+    }
+
+    private void Update()
+    {
+        // Calcola il vettore di movimento
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        movement = new Vector3(moveHorizontal, 0f, moveVertical);
+    }
+}
